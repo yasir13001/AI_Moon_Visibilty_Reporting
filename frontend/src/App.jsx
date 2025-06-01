bbb548106f5f3d1958036c627b27b0a618144fb3
said mansouri <mansouricomdz@gmail.com>
2025-06-01 20:03:35 +0100

    Setup React environment for frontend development and usage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
 import './App.css'
-import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ReportForm from './components/ReportForm';
+import TiledLayout from './components/TiledLayout';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
+      <Header/>
+      <ReportForm/>
+      <TiledLayout/>
+      <Footer/>
      </>
   )
 }
